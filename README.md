59c16c880a6e949a33a576bdc36228873d4da96e
Yannis De Cleene <yannisdecleene@icloud.com>
2018-03-27 16:43:25 +0200

    Added Todo's

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 A decentralized Patreon built on top of blockstack.
 
 The name Grazie comes from the Italian "grazie mille" meaning "thanks a million". It's a thank you from the supporters to the creators for making beautiful content but also the other way around for supporting their art.
+
+
+## TODO
+- [ ] Add multifile collection drivers
+- [ ] Add basic profile
+- [ ] Add coinbase "Subscribe with bitcoin"
+- [ ] Decide on a way to post content (embed from content provider networks or store on gaia)
+- [ ] Tip per post
+- [ ] Special post for supporters only
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -30,7 +30,7 @@ export default class Profile extends Component {
       !isSignInPending() ?
       <div className="panel-welcome" id="section-2">
         <div className="avatar-section">
-          <img src={ person.avatarUrl() ? person.avatarUrl() : avatarFallbackImage } className="img-rounded avatar" id="avatar-image" />
+          <img src={ person.avatarUrl() ? person.avatarUrl() : avatarFallbackImage } className="img-circle avatar" id="avatar-image" />
         </div>
         <h1>Hello, <span id="heading-name">{ person.name() ? person.name() : 'Nameless Person' }</span>!</h1>
         <p className="lead">
